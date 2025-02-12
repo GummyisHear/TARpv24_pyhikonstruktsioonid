1b37b1fcae0209110d6ba750875c6d2e25347f4c
Artjom Põldsaar <artjompoldsaar@gmail.com>
2025-02-12 15:44:19 +0200

    pinginaabrid

--- tund3.py
+++ tund3.py
@@ -1,49 +1,56 @@
 from random import *
 
 #Näidis 1
-arv = randint(0,10)
-print(arv)
+# arv = randint(0,10)
+# print(arv)
 
-if (arv > 5):
-    print("***********************")
-    print(f"Arv {arv} suurem kui 5")
-    print("***********************")
+# if (arv > 5):
+#     print("***********************")
+#     print(f"Arv {arv} suurem kui 5")
+#     print("***********************")
 
-if (arv > 5):
-    print(f"Arv {arv} suurem kui 5")
+# if (arv > 5):
+#     print(f"Arv {arv} suurem kui 5")
 
-# Näidis 2
-arv = randint(-10, 10)
+# # Näidis 2
+# arv = randint(-10, 10)
 
-if (arv > 0):
-    print(f"Arv {arv} on positiivne")
+# if (arv > 0):
+#     print(f"Arv {arv} on positiivne")
+# else:
+#     print(f"Arv {arv} on negatiivne") #viga!
+
+# if (arv > 0):
+#     print(f"Arv {arv} on positiivne")
+# elif (arv == 0):
+#     print("0")
+# else:
+#      print(f"Arv {arv} on negatiivne")
+
+# nimi = input("MIS ON SINU NIMI? ")
+# if (nimi.isupper() and nimi.lower() == "juku"):
+#     print("Lähme kinno")
+#     try:
+#         vanus = int(input("Kui vana sa oled? "))
+#         if vanus < 0:
+#             pilet = "!!!"
+#         elif vanus < 6:
+#             pilet="Tasuta"
+#         elif vanus <= 14:
+#             pilet = "Lastepilet"
+#         elif vanus <= 65:
+#             pilet = "Täospilet"
+#         elif vanus <= 100:
+#             pilet = "Sooduspilet"
+#         print("Sinu pilet on " + pilet)
+#     except:
+#         print("bug!")
+# else:
+#     print("Ma olen hõivatud")
+
+nimi1 = input("Mis on sinu nimi: ")
+nimi2 = input("Sisesta teine nimi: ")
+if (nimi1.isalpha() and nimi2.isalpha()):
+    input("Kas te olete pinginaabrid?")
 else:
-    print(f"Arv {arv} on negatiivne") #viga!
-
-if (arv > 0):
-    print(f"Arv {arv} on positiivne")
-elif (arv == 0):
-    print("0")
-else:
-     print(f"Arv {arv} on negatiivne")
-
-nimi = input("MIS ON SINU NIMI? ")
-if (nimi.isupper() and nimi.lower() == "juku"):
-    print("Lähme kinno")
-    try:
-        vanus = int(input("Kui vana sa oled? "))
-        if vanus < 0:
-            pilet = "!!!"
-        elif vanus < 6:
-            pilet="Tasuta"
-        elif vanus <= 14:
-            pilet = "Lastepilet"
-        elif vanus <= 65:
-            pilet = "Täospilet"
-        elif vanus <= 100:
-            pilet = "Sooduspilet"
-        print("Sinu pilet on " + pilet)
-    except:
-        print("bug!")
-else:
-    print("Ma olen hõivatud")
+    print("Nimed on valed")
