093dab237ad1a66686d6af2fef451b0e3074247d
opilane <opilane@HP-CZC2349HV0>
2025-05-14 14:03:58 +0300

    wordle update

--- wordleGui/wordle.py
+++ wordleGui/wordle.py
@@ -1,14 +1,18 @@
 import os
 import random
 from tkinter import *
+from PIL import Image, ImageTk
+from tkinter import simpledialog, messagebox
 
 BACKGROUND = "#100f27"
 GREY = "#4b4a5e"
+GREY_RGB = (75, 74, 94)
 YELLOW = "#c69000"
 GREEN = "#2f9800"
 RED = "#c4121b"
 
 sonad = [ "eesti", "sõber" ]
+gameNumber = 0
 answer = ""
 katsed = 6
 tiles = []
@@ -17,23 +21,46 @@ currentRow = 0
 
 def readFile():
     global sonad
+    global gameNumber
     sonad = []
     try:
         with open("wordleGui/sonad.txt", "r") as file:
-            for line in file:
+            for i, line in enumerate(file):
+                if (i == 0):
+                    try:
+                        n = int(line)
+                        gameNumber = n
+                        updateGameNumber()
+                        continue
+                    except:
+                        ...
                 sonad.append(line.strip())
     except FileNotFoundError:
         print("Sõnade faili ei leitud.")
         exit(1)
 
+def saveFile():
+    global sonad
+    global gameNumber
+    try:
+        with open("wordleGui/sonad.txt", "w") as file:
+            file.write(str(gameNumber) + "\n")
+            for word in sonad:
+                file.write(word + "\n")
+    except Exception as e:
+        print(f"Viga faili kirjutamisel: {e}")
+
 def startGame(root):
     global answer
     global currentKey
     global currentRow
+    global gameNumber
 
     for tile in tiles:
         resetTile(tile)
 
+    gameNumber += 1
+    updateGameNumber()
     answer = random.choice(sonad)
     currentKey = 0
     currentRow = 0
@@ -146,6 +173,7 @@ def onKey(event:Event):
     global currentRow
     global answer
     global katsed
+    global gameNumber
 
     sym = event.keysym
     char = event.char
@@ -199,10 +227,91 @@ def onKey(event:Event):
     tile.config(text=char)
     currentKey += 1
 
+def openSettings():
+    global sonad
+    WIDTH = 400
+    HEIGHT = 500
+
+    x = root.winfo_x() + (600 // 2) - (WIDTH // 2)
+    y = root.winfo_y() + (600 // 2) - (HEIGHT // 2)
+
+    window = Toplevel(root, bg=BACKGROUND)
+    window.title("Sättid")
+    window.geometry(f"{WIDTH}x{HEIGHT}+{x}+{y}")
+    window.lift()
+
+    window.grid_columnconfigure(0, weight=1)
+
+    label = Label(window, text="Muuda Sõnad", font=("Arial", 16, "bold"), bg=BACKGROUND, fg="white")
+    label.grid(row=0, column=0, pady=(20, 0), columnspan=2)
+
+    listbox = Listbox(window, width=30, height=8, selectmode=SINGLE, bg=BACKGROUND, fg="white", font=("Arial", 12))
+    listbox.config(relief="solid", highlightbackground=YELLOW, highlightcolor=YELLOW, selectbackground=YELLOW)
+    listbox.grid(row=1, column=0, columnspan=2)
+
+    def updateListbox():
+        listbox.delete(0, END)
+        for word in sonad:
+            listbox.insert(END, word)
+
+    updateListbox()
+
+    def add_word():
+        word = simpledialog.askstring("Lisa Sõna", "Sisesta uus sõna:", parent=window)
+
+        if word:
+            word = word.strip().lower()
+            if word and len(word) == 5:
+                sonad.append(word)
+                updateListbox()
+            else:
+                messagebox.showerror("Error", "Sõna peaks olema 5 tähed!")
+                window.lift()
+        saveFile()
+
+    def remove_word():
+        selected_word = listbox.curselection()
+        if selected_word:
+            selected_index = selected_word[0]
+            word_to_remove = listbox.get(selected_word)
+            sonad.remove(word_to_remove)
+            updateListbox()
+            if len(listbox.get(0, END)) > 0:
+                next_index = selected_index if selected_index < len(listbox.get(0, END)) else selected_index - 1
+                listbox.select_set(next_index)
+            else:
+                listbox.select_clear()
+        saveFile()
+
+    buttonFrame = Frame(window, bg=BACKGROUND)
+
+    button = Button(buttonFrame, text="Add Word", command=add_word)
+    button.config(relief="solid", width=16)
+    button.config(bg=GREY, fg="white", activebackground=GREEN, activeforeground="white", border=0)
+    button.grid(row=0, column=0, pady=10, padx=10)
+
+    button = Button(buttonFrame, text="Remove Word", command=remove_word)
+    button.config(relief="solid", width=16)
+    button.config(bg=GREY, fg="white", activebackground=RED, activeforeground="white", border=0)
+    button.grid(row=0, column=1, pady=10, padx=10)
+
+    buttonFrame.grid(row=2, column=0, columnspan=2)
+
+def updateGameNumber():
+    global gameNumber
+    num.config(text=f"#{gameNumber}")
+
 root = createWindow("WORDLE")
 pealkiri = Label(root, text="Eesti Wordle!", font=("Arial", 16, "bold"), bg=BACKGROUND, fg="white")
+pealkiri.pack(pady=(20, 0), padx=20, anchor="center")
 
-pealkiri.pack(pady=20, padx=20, anchor="center")
+num = Label(root, text=f"#{gameNumber}", font=("Arial", 20, "bold"), bg=BACKGROUND, fg="white")
+num.pack(pady=(5, 20), padx=20, anchor="center")
+
+icon = PhotoImage(file="wordleGui\settings.png")
+button = Button(root, image=icon, bg=BACKGROUND, border=0, highlightthickness=0, command=openSettings)
+button.config(activebackground=BACKGROUND, overrelief="groove")
+button.place(x=10, y=10)
 
 tileFrame = Frame(root, bg=BACKGROUND)
 initTiles(tileFrame)
@@ -212,3 +321,4 @@ readFile()
 startGame(root)
 
 root.mainloop()
+saveFile()
